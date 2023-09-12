821cdc81a78112bb2e677aa9c45eced57b53222c
amalsha pramod <amalshapramod1234@gmail.com>
2023-09-12 22:53:59 +0530

    login view

---
+++ forget password.swift
@@ -0,0 +1,20 @@
+//
+//  forget password.swift
+//  Personal Finance Manger
+//
+//  Created by malindu pabasara on 2023-09-11.
+//
+
+import SwiftUI
+
+struct forgotPassword_View: View {
+    var body: some View {
+        Text("omG")
+    }
+}
+
+struct forget_password_Previews: PreviewProvider {
+    static var previews: some View {
+        forgotPassword_View()
+    }
+}
